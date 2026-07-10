b601e1f287ed1f89203c1c598485c646a0d346d5
0-Cuprum-0 <imaf77198@gmail.com>
2026-07-11 00:45:35 +0200

    Not initial one, added:construction of individual alert modified:changed old structure for better readability

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dev env for python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+      cowsay
+          (python314.withPackages (ps: with ps; [
+	      gtfs-realtime-bindings
+	      requests
+	
+    ]))
+
+
+      ];
+
+      shellHook = ''
+      echo "Welcome  to dev enviroment with python and gtfs"
+      '';
+    };
+  };
+}
